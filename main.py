6cee59d3753f58efef8cc1aa11eee1935e6a1df9
Zohaib <muhammad.zohaib9613@hotmail.com>
2021-07-18 04:14:52 +0500

    updated code

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import cv2
-import numpy as np
-
-# Let's load the required XML classifiers.
-face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
-
-# Load the img in grey scale mode
-img = cv2.imread("face.jpg")
-grayScale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-# if face found, it return the position of detected faces
-faces = face_cascade.detectMultiScale(grayScale, 1.2, 3)
 
 
-for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+def opencvFaceDetection(image):
 
-    cv2.imshow("Face Detection", img)
+    faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+    # Read the image
+    img = cv2.imread(image)
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Detect faces in the image
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # Display the resulting image
+    cv2.imshow('Face Detection - Test Phase', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+opencvFaceDetection("face.jpg")
+
